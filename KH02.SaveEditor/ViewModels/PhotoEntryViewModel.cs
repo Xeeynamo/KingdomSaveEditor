55fb591c4a3922d484ca09245a2bc174ad108103
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-02-23 13:28:29 +0000

    Add photo gallery management

--- KH02.SaveEditor/ViewModels/PhotoEntryViewModel.cs
+++ KH02.SaveEditor/ViewModels/PhotoEntryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Media;
@@ -13,20 +14,21 @@ namespace KH02.SaveEditor.ViewModels
 	public class PhotoEntryViewModel : BaseNotifyPropertyChanged
 	{
 		private readonly PhotoEntry _entry;
-		private readonly int _index;
 
 		private Window Window => Application.Current.Windows.OfType<Window>().FirstOrDefault(x => x.IsActive);
+
+		public int Index { get; }
 
 		public ImageSource Image { get; set; }
 
 		public RelayCommand ExportCommand { get; }
 
-		public string Description => $"Photo #{_index + 1}";
+		public string Description => $"Photo #{Index}";
 
 		public PhotoEntryViewModel(PhotoEntry entry, int index)
 		{
 			_entry = entry;
-			_index = index;
+			Index = index + 1;
 
 			if (_entry.Length > 0)
 			{
@@ -39,16 +41,37 @@ namespace KH02.SaveEditor.ViewModels
 			ExportCommand = new RelayCommand(o =>
 			{
 				var fd = FileDialog.Factory(Window, FileDialog.Behavior.Save, ("Kingdom Hearts III Save", "bin"));
-				fd.DefaultFileName = $"Kingdom Hearts III - Photo {_index + 1}.jpg";
+				fd.DefaultFileName = $"Kingdom Hearts III - Photo {Index}.jpg";
 
 				if (fd.ShowDialog() == true)
 				{
-					using (var stream = File.Open(fd.FileName, FileMode.Create))
+					try
 					{
-						stream.Write(_entry.Data, 0, _entry.Data.Length);
+						Export(fd.FileName);
+					}
+					catch (Exception e)
+					{
+						MessageBox.Show(Window, "Unable to export the photo", "Error", MessageBoxButton.OK,
+							MessageBoxImage.Error);
 					}
 				}
 			}, x => true);
+		}
+
+		public void Export(string fileName)
+		{
+			Directory.CreateDirectory(Path.GetDirectoryName(fileName));
+			using (var stream = File.Open(fileName, FileMode.Create))
+			{
+				stream.Write(_entry.Data, 0, _entry.Data.Length);
+			}
+		}
+
+		public void Delete()
+		{
+			_entry.Length = 0;
+			_entry.Data = new byte[_entry.Data.Length];
+			OnPropertyChanged(nameof(Image));
 		}
 	}
 }
--- KH02.SaveEditor/ViewModels/PhotosViewModel.cs
+++ KH02.SaveEditor/ViewModels/PhotosViewModel.cs
@@ -1,12 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Windows;
 using KHSave.Models;
+using Xe.Tools.Wpf.Commands;
+using Xe.Tools.Wpf.Dialogs;
 using Xe.Tools.Wpf.Models;
 
 namespace KH02.SaveEditor.ViewModels
 {
 	public class PhotosViewModel : GenericListModel<PhotoEntryViewModel>
 	{
+		private Window Window => Application.Current.Windows.OfType<Window>().FirstOrDefault(x => x.IsActive);
+
+		public string Info => $"Photos: {Items.Count}";
+
+		public RelayCommand ExportAllCommand { get; }
+
+		public RelayCommand DeleteAllCommand { get; }
+
 		public PhotosViewModel(IEnumerable<PhotoEntry> list) :
 			this(list.Select((pc, index) => new PhotoEntryViewModel(pc, index)))
 		{
@@ -16,7 +29,47 @@ namespace KH02.SaveEditor.ViewModels
 		public PhotosViewModel(IEnumerable<PhotoEntryViewModel> list) :
 			base(list.Where(x => x.Image != null))
 		{
+			ExportAllCommand = new RelayCommand(o =>
+			{
+				var fd = FileDialog.Factory(Window, FileDialog.Behavior.Folder);
 
+				if (fd.ShowDialog() == true)
+				{
+					try
+					{
+						var path = fd.FileName;
+						foreach (var item in Items)
+						{
+							item.Export(Path.Combine(path, $"Kingdom Hearts III - Photo {item.Index}.jpg"));
+						}
+					}
+					catch (Exception e)
+					{
+						MessageBox.Show(Window, "Unable to export the photo", "Error", MessageBoxButton.OK,
+							MessageBoxImage.Error);
+					}
+				}
+			}, x => true);
+
+			DeleteAllCommand = new RelayCommand(o =>
+			{
+				const string msg = "Do you really want to delete all the photos?\nYou still can not save if you change your mind.";
+				var result = MessageBox.Show(Window, msg, "Delete all photos", MessageBoxButton.YesNo,
+					MessageBoxImage.Warning);
+
+				if (result == MessageBoxResult.Yes)
+				{
+					foreach (var item in Items)
+					{
+						item.Delete();
+					}
+				}
+
+				Items.Clear();
+				OnPropertyChanged(nameof(Info));
+				OnPropertyChanged(nameof(Items));
+
+			}, x => true);
 		}
 
 		protected override PhotoEntryViewModel OnNewItem()
