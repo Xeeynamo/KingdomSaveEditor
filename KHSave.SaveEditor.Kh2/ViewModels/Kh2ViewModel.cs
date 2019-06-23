190fe287a65f79ed317b06125e5b5dcb435ec6a1
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-06-23 09:38:56 +0100

    Create Player tab

--- KHSave.SaveEditor.Kh2/ViewModels/Kh2ViewModel.cs
+++ KHSave.SaveEditor.Kh2/ViewModels/Kh2ViewModel.cs
@@ -1,4 +1,22 @@
-﻿using KHSave.Lib2;
+﻿/*
+    Kingdom Hearts Save Editor
+    Copyright (C) 2019 Luciano Ciccariello
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using KHSave.Lib2;
 using KHSave.SaveEditor.Common.Contracts;
 using KHSave.SaveEditor.Common.Exceptions;
 using System.IO;
@@ -28,8 +46,11 @@ namespace KHSave.SaveEditor.Kh2.ViewModels
             }
         }
 
+        public PlayersViewModel Players { get; private set; }
+
         public void RefreshUi()
         {
+            Players = new PlayersViewModel(save);
         }
 
         public void WriteToStream(Stream stream)
