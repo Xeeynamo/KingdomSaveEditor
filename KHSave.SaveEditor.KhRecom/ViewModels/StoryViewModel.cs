40f23f683aad260ab70d0e71cd06b53fa43e9ad5
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-07-28 21:31:59 +0100

    Add unlocked card flags

--- KHSave.SaveEditor.KhRecom/ViewModels/StoryViewModel.cs
+++ KHSave.SaveEditor.KhRecom/ViewModels/StoryViewModel.cs
@@ -10,6 +10,7 @@ namespace KHSave.SaveEditor.KhRecom.ViewModels
         private StoryFlag Riku => _save.RikuStoryFlag;
         private PoohFlags Pooh => _save.PoohFlags;
         private TutorialFlags Tuto => _save.Tutorial;
+        private CardFlags Card => _save.UnlockedCards;
 
         public StoryViewModel(DataRecom save)
         {
@@ -60,5 +61,32 @@ namespace KHSave.SaveEditor.KhRecom.ViewModels
         public bool TuSavePoint { get => Tuto.SavePoint; set => Tuto.SavePoint = value; }
         public bool TuField { get => Tuto.Field; set => Tuto.Field = value; }
         public bool TuWorldSelect { get => Tuto.WorldSelect; set => Tuto.WorldSelect = value; }
+
+        public bool CaSpellbinder { get => Card.Spellbinder; set => Card.Spellbinder = value; }
+        public bool CaMetalChocobo { get => Card.MetalChocobo; set => Card.MetalChocobo = value; }
+        public bool CaLionheart { get => Card.Lionheart; set => Card.Lionheart = value; }
+        public bool CaOathkeeper { get => Card.Oathkeeper; set => Card.Oathkeeper = value; }
+        public bool CaOblivion { get => Card.Oblivion; set => Card.Oblivion = value; }
+        public bool CaUltimaWeapon { get => Card.UltimaWeapon; set => Card.UltimaWeapon = value; }
+        public bool CaDiamondDust { get => Card.DiamondDust; set => Card.DiamondDust = value; }
+        public bool CaOneWingedAngel { get => Card.OneWingedAngel; set => Card.OneWingedAngel = value; }
+        public bool CaFire { get => Card.Fire; set => Card.Fire = value; }
+        public bool CaThunder { get => Card.Thunder; set => Card.Thunder = value; }
+        public bool CaGravity { get => Card.Gravity; set => Card.Gravity = value; }
+        public bool CaStop { get => Card.Stop; set => Card.Stop = value; }
+        public bool CaAero { get => Card.Aero; set => Card.Aero = value; }
+        public bool CaSimba { get => Card.Simba; set => Card.Simba = value; }
+        public bool CaGenie { get => Card.Genie; set => Card.Genie = value; }
+        public bool CaBambi { get => Card.Bambi; set => Card.Bambi = value; }
+        public bool CaDumbo { get => Card.Dumbo; set => Card.Dumbo = value; }
+        public bool CaTinkerbell { get => Card.Tinkerbell; set => Card.Tinkerbell = value; }
+        public bool CaMushu { get => Card.Mushu; set => Card.Mushu = value; }
+        public bool CaCloud { get => Card.Cloud; set => Card.Cloud = value; }
+        public bool CaHiPotion { get => Card.HiPotion; set => Card.HiPotion = value; }
+        public bool CaMegaPotion { get => Card.MegaPotion; set => Card.MegaPotion = value; }
+        public bool CaEther { get => Card.Ether; set => Card.Ether = value; }
+        public bool CaMegaEther { get => Card.MegaEther; set => Card.MegaEther = value; }
+        public bool CaElixir { get => Card.Elixir; set => Card.Elixir = value; }
+        public bool CaMegalixir { get => Card.Megalixir; set => Card.Megalixir = value; }
     }
 }
