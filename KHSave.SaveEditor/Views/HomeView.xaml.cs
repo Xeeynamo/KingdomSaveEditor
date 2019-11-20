ddd6e85de5340ce5dc6f90df9cd4653f73f14788
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-11-20 22:20:14 +0000

    Separate main screen to HomeView

---
+++ KHSave.SaveEditor/Views/HomeView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using KHSave.SaveEditor.Services;
+using KHSave.SaveEditor.ViewModels;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KHSave.SaveEditor.Views
+{
+    /// <summary>
+    /// Interaction logic for UnloadedView.xaml
+    /// </summary>
+    public partial class HomeView : UserControl
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            Task.Run(async () =>
+            {
+                var patreonInfo = await new PatreonService(new DesktopAppIdentity()).GetPatreonInfo();
+                var patronViews = patreonInfo.Patrons
+                    .Select((patron, i) =>
+                    {
+                        return new PatronView((i + 1) / 32.0, patron.Glow)
+                        {
+                            DataContext = new PatronViewModel(patron)
+                        };
+                    })
+                    .Where(x => x != null);
+
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    patronList.Children.Clear();
+                    foreach (var patronView in patronViews)
+                    {
+                        patronList.Children.Add(patronView);
+                    }
+                });
+            });
+        }
+    }
+}
--- KHSave.SaveEditor/Views/MainWindow.xaml.cs
+++ KHSave.SaveEditor/Views/MainWindow.xaml.cs
@@ -1,12 +1,8 @@
 ﻿using KHSave.SaveEditor.Interfaces;
-using KHSave.SaveEditor.Services;
 using KHSave.SaveEditor.ViewModels;
 using System;
-using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using Unity;
 
 namespace KHSave.SaveEditor.Views
 {
@@ -31,43 +27,14 @@ namespace KHSave.SaveEditor.Views
         }
 
         private void Window_Loaded(object sender, EventArgs e)
-		{
-			Task.Run(async () =>
-			{
-				if (context.IsUpdateCheckingEnabled)
-				{
-					await context.CheckLastVersionAsync();
-				}
-			});
-
+        {
             Task.Run(async () =>
             {
-                var patreonInfo = await new PatreonService(new DesktopAppIdentity()).GetPatreonInfo();
-                var patronViews = patreonInfo.Patrons
-                    .Select((patron, i) =>
-                    {
-                        return new PatronView((i + 1) / 32.0, patron.Glow)
-                        {
-                            DataContext = new PatronViewModel(patron)
-                        };
-                    })
-                    .Where(x => x != null);
-
-                Application.Current.Dispatcher.Invoke(() =>
+                if (context.IsUpdateCheckingEnabled)
                 {
-                    patronList.Children.Clear();
-                    foreach (var patronView in patronViews)
-                    {
-                        patronList.Children.Add(patronView);
-                    }
-                });
+                    await context.CheckLastVersionAsync();
+                }
             });
-		}
-
-        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
-        {
-            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
-            e.Handled = true;
         }
     }
 }
