7c4906c5e03b87a440cd38036d8635e3895eac30
Mirrorman95 <bgiglione@hotmail.com>
2020-01-30 22:42:40 -0600

    Updating Preset.Pawn.cs as TALESIOFIFREAK did
    Updating Preset.Pawn.cs as TALESIOFIFREAK did, adding Roxas, Kairi, and Secret Boss stage Sora.

--- KHSave.Lib3/Presets/Preset.Pawn.cs
+++ KHSave.Lib3/Presets/Preset.Pawn.cs
@@ -16,10 +16,13 @@ namespace KHSave.Lib3.Presets
         public static readonly Dictionary<string, Pawn> PlayablePawns = new Dictionary<string, Pawn>
         {
             ["p_ex001"] = new Pawn { Name = "Sora (KH3)" },
+            ["p_dl001"] = new Pawn { Name = "Sora (Shibuya)" },
             ["p_ex002"] = new Pawn { Name = "Aqua" },
             ["p_ex003"] = new Pawn { Name = "Riku (KH2)" },
             ["p_ex004"] = new Pawn { Name = "Riku (KH3)" },
             ["p_ex005"] = new Pawn { Name = "Sora (KH1)" },
+            ["p_ex006"] = new Pawn { Name = "Kairi" },
+            ["p_ex007"] = new Pawn { Name = "Roxas" },
             ["p_ex011"] = new Pawn { Name = "Sora (KH2)" },
             ["p_bx001"] = new Pawn { Name = "Sora (Baymax)" },
             ["p_ca001"] = new Pawn { Name = "Sora (Caribbean)" },
