cac43f63aa2e23611d4e35bbc4579bf3dcd5562c
jeremy lorenz <48414597+TALESIOFIFREAK@users.noreply.github.com>
2020-02-09 11:39:02 +0100

    Update Preset.Pawn.cs (#25)
    add  debug mod Sora
    ["p_zz001"] = new Pawn { Name = "Sora (Debug mode)" },

--- KHSave.Lib3/Presets/Preset.Pawn.cs
+++ KHSave.Lib3/Presets/Preset.Pawn.cs
@@ -17,6 +17,7 @@ namespace KHSave.Lib3.Presets
         {
             ["p_ex001"] = new Pawn { Name = "Sora (KH3)" },
             ["p_dl001"] = new Pawn { Name = "Sora (Shibuya)" },
+            ["p_zz001"] = new Pawn { Name = "Sora (Debug mode)" },
             ["p_ex002"] = new Pawn { Name = "Aqua" },
             ["p_ex003"] = new Pawn { Name = "Riku (KH2)" },
             ["p_ex004"] = new Pawn { Name = "Riku (KH3)" },
