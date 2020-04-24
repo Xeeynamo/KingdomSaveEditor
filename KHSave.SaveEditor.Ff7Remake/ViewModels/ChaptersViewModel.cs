2fdd67b5f01c17b9f5dbbbfe98c7bab113e5e227
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-04-25 00:57:50 +0100

    Focus on the right chapter when opening a save

--- KHSave.SaveEditor.Ff7Remake/ViewModels/ChaptersViewModel.cs
+++ KHSave.SaveEditor.Ff7Remake/ViewModels/ChaptersViewModel.cs
@@ -30,12 +30,14 @@ namespace KHSave.SaveEditor.Ff7Remake.ViewModels
         public ChaptersViewModel(SaveFf7Remake save) :
             this(save.Chapters.Select((x, i) => new ChapterEntryModel(x, i)))
         {
+            CurrentChapterId = GetChapterIdFromChapterNumber(save.CurrentChapterId);
         }
 
         private ChaptersViewModel(IEnumerable<ChapterEntryModel> list) :
             base(list)
         { }
 
+        public int CurrentChapterId { get; }
         public Visibility EntryVisible => IsItemSelected ? Visibility.Visible : Visibility.Collapsed;
         public Visibility EntryNotVisible => !IsItemSelected ? Visibility.Visible : Visibility.Collapsed;
 
@@ -48,5 +50,31 @@ namespace KHSave.SaveEditor.Ff7Remake.ViewModels
 
         protected override ChapterEntryModel OnNewItem() =>
             throw new System.NotImplementedException();
+
+        private static int GetChapterIdFromChapterNumber(int chapterNumber)
+        {
+            switch (chapterNumber)
+            {
+                case 1: return 0;
+                case 2: return 1;
+                case 3: return 2;
+                case 4: return 3;
+                case 5: return 4;
+                case 6: return 5;
+                case 7: return 6;
+                case 8: return 8;
+                case 9: return 8;
+                case 10: return 9;
+                case 11: return 10;
+                case 12: return 2;
+                case 13: return 8;
+                case 14: return 8;
+                case 15: return 15;
+                case 16: return 16;
+                case 17: return 16;
+                case 18: return 16;
+                default: return -1;
+            }
+        }
     }
 }
