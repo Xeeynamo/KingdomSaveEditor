2250406a82dc9c1d5bdafe5943628deb9aeb0812
Rikux3 <d.drankov@gmail.com>
2020-04-26 13:29:17 +0200

    Add "playable" characters
    Changing the main character into someone else then Sora causes the game to behave weird, you'll die in a matter of seconds

---
+++ KHSave.Lib1/Types/PlayableCharacterType.cs
@@ -0,0 +1,19 @@
+﻿using KHSave.Attributes;
+
+namespace KHSave.Lib1.Types
+{
+    public enum PlayableCharacterType : byte
+    {
+        [Info] Sora,
+        [Info] Donald,
+        [Info] Goofy,
+        [Info] Tarzan,
+        [Unused] NotWorking,
+        [Info] Aladdin,
+        [Info] Arielle,
+        [Info] Jack,
+        [Info("Peter Pan")] PeterPan,
+        [Info] Biest,
+        [Info] Empty = 0xFF
+    }
+}
