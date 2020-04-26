7f7f3735ed0c6d396387e1134c89632e7bd9a2a9
Rikux3 <d.drankov@gmail.com>
2020-04-26 15:27:35 +0200

    Basic MVVM implementation for KH1FM save

---
+++ KHSave.SaveEditor.Kh1/ViewModels/Kh1ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using KHSave.Lib1;
+using KHSave.SaveEditor.Common.Contracts;
+using System;
+using System.IO;
+using Xe.Tools;
+
+namespace KHSave.SaveEditor.Kh1.ViewModels
+{
+    public class Kh1ViewModel : BaseNotifyPropertyChanged, IRefreshUi, IOpenStream, IWriteToStream
+    {
+        private SaveKh1.SaveFinalMix save;
+
+        public Kh1ViewModel()
+        {
+        }
+
+        public SystemViewModel System { get; private set; }
+        public PlayersViewModel Players { get; private set; }
+
+        public void RefreshUi()
+        {
+            System = new SystemViewModel(save);
+            Players = new PlayersViewModel(save);
+
+            OnPropertyChanged(nameof(System));
+            OnPropertyChanged(nameof(Players));
+        }
+
+        public void OpenStream(Stream stream)
+        {
+            save = SaveKh1.Read<SaveKh1.SaveFinalMix>(stream);
+
+            RefreshUi();
+        }
+
+        public void WriteToStream(Stream stream) => SaveKh1.Write(stream, save);
+    }
+}
--- KHSave.SaveEditor.Kh1/ViewModels/PlayerViewModel.cs
+++ KHSave.SaveEditor.Kh1/ViewModels/PlayerViewModel.cs
@@ -1,11 +1,9 @@
 ﻿using KHSave.Attributes;
 using KHSave.Lib1.Models;
 using KHSave.Lib1.Types;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using KHSave.SaveEditor.Common;
+using KHSave.SaveEditor.Common.Models;
+using System.Windows;
 
 namespace KHSave.SaveEditor.Kh1.ViewModels
 {
@@ -18,8 +16,22 @@ namespace KHSave.SaveEditor.Kh1.ViewModels
         {
             this.character = character;
             this.index = index;
+
+            Weapon = new ItemComboBoxModel<EquipmentType>(() => character.Weapon, x => character.Weapon = x);
         }
 
         public string Name => InfoAttribute.GetInfo((PlayableCharacterType)index);
+        public Visibility AdvancedVisibility => Global.IsAdvancedMode ? Visibility.Visible : Visibility.Collapsed;
+
+        public ItemComboBoxModel<EquipmentType> Weapon { get; }
+        public byte HpCur { get => character.HpCur; set => character.HpCur = value; }
+        public byte HpMax { get => character.HpMax; set => character.HpMax = value; }
+        public byte MpCur { get => character.MpCur; set => character.MpCur = value; }
+        public byte MpMax { get => character.MpMax; set => character.MpMax = value; }
+        public byte ApMax { get => character.ApMax; set => character.ApMax = value; }
+        public byte Level { get => character.Level; set => character.Level = value; }
+        public int Experience { get => character.Experience; set => character.Experience = value; }
+        public byte AccessoryCount { get => character.AccessoryCount; set => character.AccessoryCount = value; }
+        public byte ItemCount { get => character.ItemCount; set => character.ItemCount = value; }
     }
 }
