2e8543f8131e3bafb4d73b9dd66bcdfbe031a40d
Rikux3 <d.drankov@gmail.com>
2020-04-26 14:12:21 +0200

    Maybe that's Riku? Not quite sure...

---
+++ KHSave.SaveEditor.Kh1/ViewModels/PlayersViewModel.cs
@@ -0,0 +1,47 @@
+﻿using KHSave.Lib1;
+using KHSave.Lib1.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Xe.Tools.Wpf.Models;
+
+namespace KHSave.SaveEditor.Kh1.ViewModels
+{
+    public class PlayersViewModel : GenericListModel<PlayerViewModel>
+    {
+        private readonly SaveKh1.SaveFinalMix save;
+
+        public PlayersViewModel(SaveKh1.SaveFinalMix save) :
+            this(save.Characters)
+        {
+            this.save = save;
+        }
+
+        public PlayersViewModel(IEnumerable<Character> list) :
+            this(list.Select((pc, index) => new PlayerViewModel(pc, index)))
+        {
+
+        }
+
+        public PlayersViewModel(IEnumerable<PlayerViewModel> list) :
+            base(list)
+        {
+
+        }
+
+        public Visibility PlayerVisible => IsItemSelected ? Visibility.Visible : Visibility.Collapsed;
+        public Visibility PlayerNotVisible => !IsItemSelected ? Visibility.Visible : Visibility.Collapsed;
+
+        protected override void OnSelectedItem(PlayerViewModel item)
+        {
+            base.OnSelectedItem(item);
+            OnPropertyChanged(nameof(PlayerVisible));
+            OnPropertyChanged(nameof(PlayerNotVisible));
+        }
+
+        protected override PlayerViewModel OnNewItem()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
