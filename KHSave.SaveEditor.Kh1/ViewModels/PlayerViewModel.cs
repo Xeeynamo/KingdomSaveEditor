c9972d5587e3b91688bd98f1a7298d175daeaba0
Rikux3 <d.drankov@gmail.com>
2020-04-26 14:26:08 +0200

    Rename some fields to make my life easier

--- KHSave.SaveEditor.Kh1/ViewModels/PlayerViewModel.cs
+++ KHSave.SaveEditor.Kh1/ViewModels/PlayerViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using KHSave.Attributes;
+using KHSave.Lib1.Models;
+using KHSave.Lib1.Types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,15 @@ namespace KHSave.SaveEditor.Kh1.ViewModels
 {
     public class PlayerViewModel
     {
+        private readonly Character character;
+        private readonly int index;
+
+        public PlayerViewModel(Character character, int index)
+        {
+            this.character = character;
+            this.index = index;
+        }
+
+        public string Name => InfoAttribute.GetInfo((PlayableCharacterType)index);
     }
 }
