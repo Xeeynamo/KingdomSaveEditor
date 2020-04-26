617fceede7cd10f8aa01eaeb09f5be7246235885
Rikux3 <d.drankov@gmail.com>
2020-04-26 15:59:56 +0200

    Expand EquipmentItem viewmodels and factories to hold 12 items

---
+++ KHSave.SaveEditor.Kh1/Service/EquipmentManagerFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KHSave.Lib1.Models;
+using KHSave.Lib1.Types;
+
+namespace KHSave.SaveEditor.Kh1.Service
+{
+    public static class EquipmentManagerFactory
+    {
+        private class AccessoryEquipmentManager : IEquipmentManager
+        {
+            private readonly Character character;
+
+            internal AccessoryEquipmentManager(Character character)
+            {
+                this.character = character;
+            }
+
+            public byte Count { get => character.AccessoryCount; set => character.AccessoryCount = Math.Min(byte.MaxValue, value); }
+
+            public EquipmentType GetEquipment(byte index) => (EquipmentType)character.Accessories[index];
+            public void SetEquipment(byte index, EquipmentType equipment) => character.Accessories[index] = (byte)equipment;
+        }
+
+        private class ConsumableEquipmentManager : IEquipmentManager
+        {
+            private readonly Character character;
+
+            internal ConsumableEquipmentManager(Character character)
+            {
+                this.character = character;
+            }
+
+            public byte Count { get => character.ItemCount; set => character.ItemCount = Math.Min(byte.MaxValue, value); }
+
+            public EquipmentType GetEquipment(byte index) => (EquipmentType)character.Items[index];
+            public void SetEquipment(byte index, EquipmentType equipment) => character.Items[index] = (byte)equipment;
+        }
+
+        public static IEquipmentManager ForAccessory(Character character) => new AccessoryEquipmentManager(character);
+        public static IEquipmentManager ForConsumable(Character character) => new ConsumableEquipmentManager(character);
+    }
+}
---
+++ KHSave.SaveEditor.Kh1/Service/IEquipmentManager.cs
@@ -0,0 +1,12 @@
+﻿using KHSave.Lib1.Types;
+
+namespace KHSave.SaveEditor.Kh1.Service
+{
+    public interface IEquipmentManager
+    {
+        byte Count { get; set; }
+
+        EquipmentType GetEquipment(byte index);
+        void SetEquipment(byte index, EquipmentType equipment);
+    }
+}
