8e283fe7717269f36e7b4a49cc956162c4aa8b0b
Rikux3 <d.drankov@gmail.com>
2020-04-26 13:27:54 +0200

    Add shortcut commands (magic only)

---
+++ KHSave.Lib1/Types/CommandType.cs
@@ -0,0 +1,16 @@
+﻿using KHSave.Attributes;
+
+namespace KHSave.Lib1.Types
+{
+    public enum CommandType : byte
+    {
+        [Magic] Fire,
+        [Magic] Blizzard,
+        [Magic] Thunder,
+        [Magic] Cure,
+        [Magic] Gravity,
+        [Magic] Stop,
+        [Magic] Aero,
+        Empty = 0xFF
+    }
+}
