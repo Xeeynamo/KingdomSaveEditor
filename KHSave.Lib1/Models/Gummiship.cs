725e789e587432755689da44173cd4f0e60c5c5a
Christophe Huybrechts <huybchris@hotmail.com>
2020-05-09 23:16:49 +0200

    Adding Gummi Blocks Types and reading of Gummi Ships

---
+++ KHSave.Lib1/Models/Gummiship.cs
@@ -0,0 +1,15 @@
+﻿using KHSave.Lib1.Types;
+using Xe.BinaryMapper;
+
+namespace KHSave.Lib1.Models
+{
+    public class Gummiship
+    {
+        [Data] public byte BlocksUsed { get; set; }
+        //probably the other statistics
+        [Data(Count = 75)] public byte[] Unk01 { get; set; }
+        [Data(Count = 10)] public byte[] ShipName { get; set; }
+        //contains block ID, rotations, color?
+        [Data(Count = 3866)] public byte[] Unk02 { get; set; }
+    }
+}
