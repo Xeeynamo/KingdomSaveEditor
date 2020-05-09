de2e633e9a7396c949963ca74cece435137a3e55
Rikux3 <d.drankov@gmail.com>
2020-05-09 19:29:35 +0200

    Update Worlds with Table from OpenKh

--- KHSave.LibBbs/Types/WorldType.cs
+++ KHSave.LibBbs/Types/WorldType.cs
@@ -2,22 +2,25 @@
 
 namespace KHSave.LibBbs.Types
 {
+    // https://openkh.dev/bbs/worlds.html
     public enum WorldType : byte
     {
-        [World("dp", "Land of Departure")] LandofDeparture = 1,
-        [World("sw", "Dwarf Woodlands")] DwarfWoodlands = 2,
-        [World("cd", "Castle of Dreams")] CastleofDreams = 3,
-        [World("sb", "Enchanted Dominion")] EnchantedDominion = 4,
-        [World("yt", "The Mysterious Tower")] MysteriousTower = 5,
-        [World("rg", "Radiant Garden")] RadiantGarden = 6,
-        [World("jb", "Realm of Darkness / Miscellaneous")] RealmOfDarkness = 7,
-        [World("he", "Olympus Coliseum")] Olympus = 8,
-        [World("ls", "Deep Space")] DeepSpace = 9,
-        [World("di", "Destiny Islands")] DestinyIslands = 0xA, // 0x0A
-        [World("pp", "Neverland")] Neverland = 0xB, // 0x0B
-        [World("dc", "Disney Town")] DisneyTown = 0xC, // 0x0C
-        [World("kg", "Keyblade Graveyard")] KeybladeGraveyard = 0xD, // 0x0D
-        [World("vs", "Mirage Arena")] MirageArena = 0xF, // 0x0F
-        [World("wm", "World Map")] WorldMap = 0x11, // 0x11
+        [World("ex", "Generic")] Generic = 0x0,
+        [World("dp", "Land of Departure")] LandofDeparture = 0x1,
+        [World("sw", "Dwarf Woodlands")] DwarfWoodlands = 0x2,
+        [World("cd", "Castle of Dreams")] CastleofDreams = 0x3,
+        [World("sb", "Enchanted Dominion")] EnchantedDominion = 0x4,
+        [World("yt", "The Mysterious Tower")] MysteriousTower = 0x5,
+        [World("rg", "Radiant Garden")] RadiantGarden = 0x6,
+        [World("jb", "Jungle Book (JP) / Miscellaneous / Realm of Darkness (FM)")] RealmOfDarkness = 0x7,
+        [World("he", "Olympus Coliseum")] Olympus = 0x8,
+        [World("ls", "Deep Space")] DeepSpace = 0x9,
+        [World("di", "Destiny Island")] DestinyIsland = 0xA,
+        [World("pp", "Neverland")] Neverland = 0xB,
+        [World("dc", "Disney Town")] DisneyTown = 0xC,
+        [World("kg", "Keyblade Graveyard")] KeybladeGraveyard = 0xD,
+        [World("vs", "Mirage Arena")] MirageArena = 0xF,
+        [World("bd", "Command Board")] CommandBoard = 0x10,
+        [World("wm", "World Map")] WorldMap = 0x11,
     }
 }
