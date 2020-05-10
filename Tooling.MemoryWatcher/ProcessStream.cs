144cf30853387bdbaf93aa5d18d6f7763b9e5dbd
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-05-10 20:56:21 +0100

    Add tool to watch memory changes to PCSX2

---
+++ Tooling.MemoryWatcher/ProcessStream.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tooling.MemoryWatcher
+{
+	public class ProcessStream : Stream
+	{
+		[DllImport("kernel32.dll")]
+		private static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+		[DllImport("kernel32.dll")]
+		private static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out int BytesRead);
+
+		[DllImport("kernel32.dll")]
+		static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, out int lpNumberOfBytesWritten);
+		
+		[DllImport("advapi32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool OpenProcessToken(IntPtr ProcessHandle, UInt32 DesiredAccess, out IntPtr TokenHandle);
+
+
+		private Process _process;
+		private IntPtr _hProcess;
+		private long position;
+
+		public ProcessStream(Process process, uint baseAddress, uint length)
+		{
+			BaseAddress = baseAddress;
+			Length = length;
+
+			OpenProcess(process);
+		}
+
+
+		public uint BaseAddress { get; }
+		public override bool CanRead => true;
+
+		public override bool CanSeek => true;
+
+		public override bool CanWrite => true;
+
+		public override long Length { get; }
+
+		public override long Position
+		{
+			get => position;
+			set => position = value;
+		}
+
+		public override void Flush()
+		{
+		}
+
+		public override int Read(byte[] buffer, int offset, int count)
+		{
+			int read;
+			var actualCount = (int)Math.Min(count, Math.Max(0, Length - Position));
+			var pos = (IntPtr)(BaseAddress + Position);
+
+			if (offset == 0)
+			{
+				ReadProcessMemory(_hProcess, pos, buffer, actualCount, out read);
+			}
+			else
+			{
+				byte[] data = new byte[actualCount];
+				ReadProcessMemory(_hProcess, pos, buffer, actualCount, out read);
+				Array.Copy(data, 0, buffer, offset, read);
+			}
+
+			Position += read;
+
+			return read;
+		}
+
+		public override long Seek(long offset, SeekOrigin origin)
+		{
+			switch (origin)
+			{
+				case SeekOrigin.Begin:
+					return Position = offset;
+				case SeekOrigin.Current:
+					return Position += offset;
+				case SeekOrigin.End:
+					return Position = Length + offset;
+				default:
+					return Position;
+			}
+		}
+
+		public override void SetLength(long value)
+		{
+		}
+
+		public override void Write(byte[] buffer, int offset, int count)
+		{
+			var actualCount = (int)Math.Min(count, Math.Max(0, Length - Position));
+			var pos = (IntPtr)(BaseAddress + Position);
+
+			int written;
+			if (offset == 0)
+			{
+				WriteProcessMemory(_hProcess, pos, buffer, actualCount, out written);
+			}
+			else
+			{
+				var data = new byte[count];
+				Array.Copy(buffer, offset, data, 0, count);
+				WriteProcessMemory(_hProcess, pos, data, actualCount, out written);
+			}
+
+			Position += written;
+		}
+
+		private void OpenProcess(Process process)
+		{
+			const int TOKEN_QUERY = 8;
+			const int PROCESS_ALL_ACCESS = 0x000F0000 | 0x00100000 | 0xFFF;
+			_hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, process.Id);
+			_process = process;
+
+			var isSuccess = OpenProcessToken(_hProcess, TOKEN_QUERY, out var tokenHandle);
+		}
+
+		public static IEnumerable<Process> GetProcesses() =>
+			Process.GetProcesses();
+
+		public static Process TryGetProcess(Func<Process, bool> predicate, int timeout = 10000, int sleep = 100)
+		{
+			var stopwatch = new Stopwatch();
+			stopwatch.Start();
+
+			do
+			{
+				var process = GetProcesses().FirstOrDefault(predicate);
+				if (process != null)
+					return process;
+
+				Thread.Sleep(sleep);
+			} while (stopwatch.ElapsedMilliseconds < timeout);
+
+			return null;
+		}
+
+		public static Task<Process> TryGetProcessAsync(Func<Process, bool> predicate, int timeout = 10000, int sleep = 100) =>
+			Task.Run(() => TryGetProcess(predicate, timeout, sleep));
+	}
+}
---
+++ Tooling.MemoryWatcher/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tooling.MemoryWatcher
+{
+    public static class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var configFileName = Path.GetFullPath(args.FirstOrDefault() ?? "config.yml");
+            var configuration = Configuration.ReadFromYaml(configFileName);
+
+            Console.WriteLine("List of supported games:");
+            for (var i = 0; i < configuration.Games.Count; i++)
+            {
+                var game = configuration.Games[i];
+                Console.WriteLine($" {i:D2}. [{game.Process}] {game.Name}");
+            }
+
+            int selectedGame;
+            do
+            {
+                Console.Write("What game do you want to watch? (eg. 3): ");
+                if (!int.TryParse(Console.ReadLine(), out selectedGame))
+                {
+                    Console.WriteLine("ERROR: You did not typed a valid number?");
+                    selectedGame = -1;
+                }
+
+                if (selectedGame >= configuration.Games.Count)
+                {
+                    Console.WriteLine("The game you selected does not exist.");
+                    selectedGame = -1;
+                }
+            } while (selectedGame < 0);
+
+            try
+            {
+                await Run(configFileName, configuration.Games[selectedGame]);
+            }
+            catch (ExecutionException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        
+        async static Task Run(string configFileName, GameConfiguration game)
+        {
+            var processName = game.Process;
+            Console.WriteLine($"Now searching for the process {processName}...");
+
+            var process = await ProcessStream
+                .TryGetProcessAsync(x => x.ProcessName.Contains(processName));
+            if (process == null)
+                throw new ExecutionException($"Failed to attach to {game.Process} in the time limit.");
+
+            Console.WriteLine($"We suppose that {processName} has {game.Name} running.\n");
+            Console.WriteLine(" [c] clear console");
+            Console.WriteLine(" [d] dump memory to file");
+            Console.WriteLine(" [p] pause/resume watch");
+            Console.WriteLine(" [x] exit");
+            Console.WriteLine("Now watching to memory differences...");
+
+            bool isWatchRunning = true;
+            bool isSupposedToExit = false;
+
+            using var stream = new ProcessStream(process, game.Offset, game.Length);
+            var taskInput = Task.Run(async () =>
+            {
+                while (!isSupposedToExit)
+                {
+                    switch (Console.ReadKey().KeyChar)
+                    {
+                        case 'c':
+                            Console.Clear();
+                            break;
+                        case 'd':
+                            lock (stream)
+                                Dump(stream);
+                            break;
+                        case 'p':
+                            isWatchRunning = !isWatchRunning;
+                            if (isWatchRunning)
+                                Console.WriteLine("Memory watcher is now scheduled to resume its watch!");
+                            else
+                                Console.WriteLine("Memory watcher is now scheduled to take a rest.");
+                            break;
+                        case 'x':
+                            isSupposedToExit = true;
+                            Console.WriteLine("Memory watcher is now scheduled to exit.");
+                            break;
+                    }
+
+                    await Task.Delay(10);
+                }
+            });
+
+            var taskWatch = Task.Run(async () =>
+            {
+                var length = game.Length;
+                var buffer = new byte[length];
+                var current = new byte[length];
+
+                stream.Position = 0;
+                stream.Read(buffer, 0, (int)length);
+
+                while (!isSupposedToExit)
+                {
+                    await Task.Delay(1);
+                    if (!isWatchRunning)
+                        continue;
+
+                    lock (stream)
+                    {
+                        stream.Position = 0;
+                        stream.Read(current);
+                    }
+
+                    for (var i = 0U; i < length; i++)
+                    {
+                        if (buffer[i] != current[i])
+                            LogDifference(i, buffer[i], current[i], game.Labels);
+                    }
+
+                    var tmp = buffer; // flip buffer for performance reasons
+                    buffer = current;
+                    current = tmp;
+
+                }
+            });
+
+            using var fsWatcher = new FileSystemWatcher()
+            {
+                Path = Path.GetDirectoryName(configFileName),
+                Filter = Path.GetFileName(configFileName),
+                NotifyFilter = NotifyFilters.LastWrite,
+                EnableRaisingEvents = true,
+            };
+            fsWatcher.Changed += (object sender, FileSystemEventArgs e) =>
+            {
+                Console.WriteLine($"The configuration file {e.Name} has been changed and it will be reloaded.");
+                Thread.Sleep(50);
+
+                try
+                {
+                    var configuration = Configuration.ReadFromYaml(configFileName);
+                    var gameNew = configuration.Games
+                        .FirstOrDefault(x => x.Name == game.Name);
+
+                    if (gameNew == null)
+                        Console.WriteLine($"The configuration for the running game can not be found, so it will not be reloaded.");
+                    else
+                        game = gameNew;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred while reading the configuration: {ex.Message}.");
+                }
+            };
+
+            await Task.WhenAll(taskInput, taskWatch);
+        }
+
+        private static void Dump(ProcessStream stream)
+        {
+            var fileName = $"dump_{DateTime.Now.Ticks}.bin";
+
+            using var outStream = File.Create(fileName);
+            stream.Position = 0;
+            stream.CopyTo(outStream);
+
+            Console.WriteLine($"Dumped to {fileName}");
+        }
+
+        private static void LogDifference(uint offset, byte previous, byte current, List<GameLabelConfiguration> labels)
+        {
+            var label = GetLabel(labels, offset);
+            if (!(label?.Hidden == true))
+                Console.WriteLine($"0x{offset:X06} {previous:X02} > {current:X02}  {label?.Comment}");
+
+        }
+
+        private static GameLabelConfiguration GetLabel(List<GameLabelConfiguration> labels, uint offset)
+        {
+            if (labels == null)
+                return null;
+
+            foreach (var label in labels)
+            {
+                if (offset >= label.Offset && offset <= label.Offset + label.Length)
+                    return label;
+            }
+
+            return null;
+        }
+
+        static IEnumerable<int> IgnoreOffset(int offset)
+        {
+            yield return offset;
+        }
+
+        static IEnumerable<int> IgnoreOffset(int startOffset, int count) =>
+            Enumerable.Range(startOffset, count);
+    }
+
+    public class ExecutionException : Exception
+    {
+        public ExecutionException(string message) :
+            base(message)
+        { }
+    }
+}
