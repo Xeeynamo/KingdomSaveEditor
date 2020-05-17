837fc86e58cc8a9f9141a49be14be901a9cb38f9
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-05-18 00:58:46 +0100

    KH2: Parse story progression

---
+++ KHSave.Lib2/Models/Progress.cs
@@ -0,0 +1,19 @@
+﻿using Xe.BinaryMapper;
+
+namespace KHSave.Lib2.Models
+{
+    public class Progress
+    {
+        [Data(Count = 0x20)] public byte[] Flags { get; set; }
+
+        public bool GetFlag(int index) => (Flags[index / 8] & (1 << (index % 8))) != 0;
+        public void SetFlag(int index, bool value)
+        {
+            var mask = (byte)(1 << (index % 8));
+            if (value)
+                Flags[index / 8] |= mask;
+            else
+                Flags[index / 8] &= (byte)~mask;
+        }
+    }
+}
--- KHSave.Lib2/SaveKh2.FinalMix.cs
+++ KHSave.Lib2/SaveKh2.FinalMix.cs
@@ -18,6 +18,8 @@ namespace KHSave.Lib2
             [Data] public byte SpawnId { get; set; }
             [Data] public byte Unused0f { get; set; }
 
+            [Data(0x1c90, Count = 20, Stride = 0x20)] public byte StoryProgress { get; set; }
+            // 3e8 of what?
             [Data(0x22f8, Count = 8 * Constants.WorldCount)] public byte[] RoomVisitedFlag { get; set; } // There might be a chance that it starts from 0x2300
             [Data(0x2440)] public int MunnyAmount { get; set; }
             [Data(0x2494)] public int Timer { get; set; }
