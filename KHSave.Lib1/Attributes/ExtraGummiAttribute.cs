0fa6d56cadc6d5a474c07a9cbdb61dc02cb1f67a
Christophe Huybrechts <huybchris@hotmail.com>
2020-05-18 02:28:27 +0200

    Fix ExtraGummiAttribute and added BlueprintGummi Attribute

--- KHSave.Lib1/Attributes/ExtraGummiAttribute.cs
+++ KHSave.Lib1/Attributes/ExtraGummiAttribute.cs
@@ -1,14 +1,23 @@
-﻿using System;
+﻿using KHSave.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace KHSave.Lib1.Attributes
 {
-    public class ExtraGummiAttribute : GummiBlockAttribute
+    public class ExtraGummiAttribute : InfoAttribute
     {
-        public ExtraGummiAttribute(string info, int sizeX, int sizeY, int sizeZ, int armor, int max,
-            int price = 0, int sell = 0, string description = null) : base(info, sizeX, sizeY, sizeZ,
-                armor, max, price, sell, description)
-        { }
+        public int Max { get; set; }
+        public int Price { get; set; }
+        public int Sell { get; set; }
+        public string Description { get; set; }
+
+        public ExtraGummiAttribute(string info, int max, int price = 0, int sell = 0, string description = null) : base(info)
+        {
+            Max = max;
+            Price = price;
+            Sell = sell;
+            Description = description;
+        }
     }
 }
---
+++ KHSave.Lib1/Types/GameTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KHSave.Lib1.Types
+{
+    [Flags]
+    public enum GameTypes
+    {
+        None,
+        Jp = 1,
+        EuUs = 2,
+        Fm = 4,
+        All = Jp | EuUs | Fm
+    }
+}
