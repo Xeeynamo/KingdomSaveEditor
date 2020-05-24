3d9a537b612317def06fc0d8ea6c874b693f98e0
Rikux3 <d.drankov@gmail.com>
2020-05-24 16:37:57 +0200

    If command in deck is empty, display that properly in the UI

--- KHSave.SaveEditor.KhBbs/ViewModels/DeckViewModel.cs
+++ KHSave.SaveEditor.KhBbs/ViewModels/DeckViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using KHSave.LibBbs.Models;
 using KHSave.SaveEditor.KhBbs.Models;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using Xe.Tools;
 
@@ -17,30 +16,37 @@ namespace KHSave.SaveEditor.KhBbs.ViewModels
             CommandList = commandList;
         }
 
-        public string Name => Encoding.GetEncoding(932).GetString(deck.Name); //shift-jis, does this also apply to other versions?
+        public string Name => Encoding.GetEncoding(932).GetString(deck.Name); //shift-jis, seems to work in european saves as well
 
-        public ushort Bc1 { get => deck.BattleCommands[0].Id; set => deck.BattleCommands[0].Id = value; }
-        public ushort Bc2 { get => deck.BattleCommands[1].Id; set => deck.BattleCommands[1].Id = value; }
-        public ushort Bc3 { get => deck.BattleCommands[2].Id; set => deck.BattleCommands[2].Id = value; }
-        public ushort Bc4 { get => deck.BattleCommands[3].Id; set => deck.BattleCommands[3].Id = value; }
-        public ushort Bc5 { get => deck.BattleCommands[4].Id; set => deck.BattleCommands[4].Id = value; }
-        public ushort Bc6 { get => deck.BattleCommands[5].Id; set => deck.BattleCommands[5].Id = value; }
-        public ushort Bc7 { get => deck.BattleCommands[6].Id; set => deck.BattleCommands[6].Id = value; }
-        public ushort Bc8 { get => deck.BattleCommands[7].Id; set => deck.BattleCommands[7].Id = value; }
+        public short BattleCommand1 { get => GetValue(deck.BattleCommands[0].Id); set => deck.BattleCommands[0].Id = (ushort)value; }
+        public short BattleCommand2 { get => GetValue(deck.BattleCommands[1].Id); set => deck.BattleCommands[1].Id = (ushort)value; }
+        public short BattleCommand3 { get => GetValue(deck.BattleCommands[2].Id); set => deck.BattleCommands[2].Id = (ushort)value; }
+        public short BattleCommand4 { get => GetValue(deck.BattleCommands[3].Id); set => deck.BattleCommands[3].Id = (ushort)value; }
+        public short BattleCommand5 { get => GetValue(deck.BattleCommands[4].Id); set => deck.BattleCommands[4].Id = (ushort)value; }
+        public short BattleCommand6 { get => GetValue(deck.BattleCommands[5].Id); set => deck.BattleCommands[5].Id = (ushort)value; }
+        public short BattleCommand7 { get => GetValue(deck.BattleCommands[6].Id); set => deck.BattleCommands[6].Id = (ushort)value; }
+        public short BattleCommand8 { get => GetValue(deck.BattleCommands[7].Id); set => deck.BattleCommands[7].Id = (ushort)value; }
 
-        public ushort Ac1 { get => deck.ActionCommands[0].Id; set => deck.ActionCommands[0].Id = value; }
-        public ushort Ac2 { get => deck.ActionCommands[1].Id; set => deck.ActionCommands[1].Id = value; }
-        public ushort Ac3 { get => deck.ActionCommands[2].Id; set => deck.ActionCommands[2].Id = value; }
-        public ushort Ac4 { get => deck.ActionCommands[3].Id; set => deck.ActionCommands[3].Id = value; }
-        public ushort Ac5 { get => deck.ActionCommands[4].Id; set => deck.ActionCommands[4].Id = value; }
-        public ushort Ac6 { get => deck.ActionCommands[5].Id; set => deck.ActionCommands[5].Id = value; }
-        public ushort Ac7 { get => deck.ActionCommands[6].Id; set => deck.ActionCommands[6].Id = value; }
-        public ushort Ac8 { get => deck.ActionCommands[7].Id; set => deck.ActionCommands[7].Id = value; }
-        public ushort Ac9 { get => deck.ActionCommands[8].Id; set => deck.ActionCommands[8].Id = value; }
-        public ushort Ac10 { get => deck.ActionCommands[9].Id; set => deck.ActionCommands[9].Id = value; }
+        public short ActionCommand1 { get => GetValue(deck.ActionCommands[0].Id); set => deck.ActionCommands[0].Id = (ushort)value; }
+        public short ActionCommand2 { get => GetValue(deck.ActionCommands[1].Id); set => deck.ActionCommands[1].Id = (ushort)value; }
+        public short ActionCommand3 { get => GetValue(deck.ActionCommands[2].Id); set => deck.ActionCommands[2].Id = (ushort)value; }
+        public short ActionCommand4 { get => GetValue(deck.ActionCommands[3].Id); set => deck.ActionCommands[3].Id = (ushort)value; }
+        public short ActionCommand5 { get => GetValue(deck.ActionCommands[4].Id); set => deck.ActionCommands[4].Id = (ushort)value; }
+        public short ActionCommand6 { get => GetValue(deck.ActionCommands[5].Id); set => deck.ActionCommands[5].Id = (ushort)value; }
+        public short ActionCommand7 { get => GetValue(deck.ActionCommands[6].Id); set => deck.ActionCommands[6].Id = (ushort)value; }
+        public short ActionCommand8 { get => GetValue(deck.ActionCommands[7].Id); set => deck.ActionCommands[7].Id = (ushort)value; }
+        public short ActionCommand9 { get => GetValue(deck.ActionCommands[8].Id); set => deck.ActionCommands[8].Id = (ushort)value; }
+        public short ActionCommand10 { get => GetValue(deck.ActionCommands[9].Id); set => deck.ActionCommands[9].Id = (ushort)value; }
 
-        public ushort Shotlock { get => deck.Shotlock.Id; set => deck.Shotlock.Id = value; }
+        public short Shotlock { get => GetValue(deck.Shotlock.Id); set => deck.Shotlock.Id = (ushort)value; }
 
         public IEnumerable<CommandListModel> CommandList { get; set; }
+
+        private short GetValue(ushort value)
+        {
+            if (value == 0xffff)
+                return -1;
+            else return (short)value;
+        }
     }
 }
