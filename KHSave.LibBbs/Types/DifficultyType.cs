1a5c6051fe64b07f32a44b4a2608304470a93e95
Rikux3 <d.drankov@gmail.com>
2020-05-24 16:34:10 +0200

    Remove the second Standard difficulty value, it's confusing

--- KHSave.LibBbs/Types/DifficultyType.cs
+++ KHSave.LibBbs/Types/DifficultyType.cs
@@ -6,7 +6,7 @@ namespace KHSave.LibBbs.Types
     {
         [Info] Beginner = 0x8,
         [Info] Standard = 0x48,
-        [Info("Standard")] Standard2 = 0x49, //weird thing on the european games, save this for now
+        //[Info("Standard")] Standard2 = 0x49, //weird thing on the european games, save this for now
         [Info] Proud = 0x88,
         [Info] Critical = 0xC8,
     }
