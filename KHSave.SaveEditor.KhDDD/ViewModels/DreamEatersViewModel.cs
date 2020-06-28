21a139d37437cbd7c63e9724a61a3b6f96cbaa57
Christophe Huybrechts <huybchris@hotmail.com>
2020-06-28 03:01:52 +0200

    Some progress towards dream eaters support

---
+++ KHSave.SaveEditor.KhDDD/ViewModels/DreamEatersViewModel.cs
@@ -0,0 +1,15 @@
+﻿using KHSave.LibDDD.Model;
+using System.Linq;
+using Xe.Tools.Wpf.Models;
+
+namespace KHSave.SaveEditor.KhDDD.ViewModels
+{
+    public class DreamEatersViewModel : GenericListModel<DreamEaterViewModel>
+    {
+        public DreamEatersViewModel(DreamEater[] dreamEaters):
+            base(dreamEaters.Select(x => new DreamEaterViewModel(x)))
+        {
+
+        }
+    }
+}
