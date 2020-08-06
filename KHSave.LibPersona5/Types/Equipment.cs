e10010147b9415c681958dcd638fd23376c60cb5
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-08-06 23:41:42 +0200

    P5: Add more equip

--- KHSave.LibPersona5/Types/Equipment.cs
+++ KHSave.LibPersona5/Types/Equipment.cs
@@ -17,6 +17,11 @@ namespace KHSave.LibPersona5.Types
         public PoleAttribute(string name = null) : base(name) { }
     }
 
+    public class SaberAttribute : EquipmentAttribute
+    {
+        public SaberAttribute(string name = null) : base(name) { }
+    }
+
     public class RangeAttribute : EquipmentAttribute
     {
         public RangeAttribute(string name = null) : base(name) { }
@@ -25,6 +30,11 @@ namespace KHSave.LibPersona5.Types
     public class RangeJokerAttribute : RangeAttribute
     {
         public RangeJokerAttribute(string name = null) : base(name) { }
+    }
+
+    public class RangeCrowAttribute : RangeAttribute
+    {
+        public RangeCrowAttribute(string name = null) : base(name) { }
     }
 
     public class ProtectorAttribute : EquipmentAttribute
@@ -59,29 +69,39 @@ namespace KHSave.LibPersona5.Types
 
     public enum Equipment : ushort
     {
+        [Dagger("Generic Gear")] GenericGear = 0x0001,
         [Dagger("Rebel Knife")] RebelKnife = 0x0002,
         [Dagger("Frenzy Dagger")] FrenzyDagger = 0x0016,
         [Pole("Dragon God Pole")] DragonGodPole = 0x0025,
         [Dagger("Black Kogatana")] BlackKogatana = 0x00eb,
+        [Saber("Lumina Saber")] LuminaSaber = 0xdf,
         [ProtectorMale("Dark Undershirt")] DarkUndershirt = 0x1002,
         [ProtectorCat("Gorgeous Collar")] GorgeousCollar = 0x1004,
         [ProtectorFemale("Titanium Set")] TitaniumSet = 0x1005,
+        [ProtectorMale("Formal Shirt")] FormalShirt = 0x100A,
         [ProtectorMale("UFO Vest")] UfoVest = 0x1060,
         [ProtectorMale("Moon Man Vest")] MoonManVest = 0x1068,
+        [ProtectorFemale("Gambler Dress")] GamblerDress = 0x107D,
         [ProtectorMale("Fireman Happi")] FiremanHappi = 0x108c,
         [Accessory("Hip Glasses")] HipGlasses = 0x2001,
         [Accessory("Gale Cape")] GaleCape = 0x2002,
         [Accessory("Blitz Ring")] BlitzRing = 0x2003,
         [Accessory("Druid Amulet")] DruidAmulet = 0x2004,
         [Accessory("Sonic Socks")] SonicSocks = 0x2005,
+        [Accessory("Black Necktie")] BlackNecktie = 0x2009,
         [Accessory("Stamina Sash")] StaminaSash = 0x200c,
+        [Accessory("Ignis Ring")] IgnisRing = 0x2076,
         [Accessory("Koh-i-Noor")] KohiNoor = 0x20ba,
         [RangeJoker("Cocytus")] Cocytus = 0x80fa,
         [Outfit("Phantom Suit")] PhantomSuit = 0x7002,
         [Outfit("Pirate Armor")] PirateArmor = 0x7003,
-        [Outfit("Red Latex Suit")] RedLatexSuit = 0x7005,
         [Outfit("Morgana Classic")] MorganaClassic = 0x7004,
+        [Outfit("Red Latex Suit")] RedLatexSuit = 0x7005,
+        [Outfit("Cyber Gear")] CyberGear = 0x7009,
+        [Outfit("Prince Suit")] PrinceSuit = 0x700A,
         [Outfit("Swimsuit")] Swimsuit = 0x7038,
         [RangeJoker("Tkachev II")] Tkachev2 = 0x8013,
+        [RangeJoker("Aura Gun 1")] AuraGun1 = 0x8008,
+        [RangeCrow("Moebius")] Moebius = 0x80E0,
     }
 }
