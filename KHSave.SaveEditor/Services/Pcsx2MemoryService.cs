5e56b726aacb6b1b484678da049e46460aa81636
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-08-25 08:22:26 +0200

    Add more game codes for PCSX2 functionality

--- KHSave.SaveEditor/Services/Pcsx2MemoryService.cs
+++ KHSave.SaveEditor/Services/Pcsx2MemoryService.cs
@@ -33,21 +33,33 @@ namespace KHSave.SaveEditor.Services
             new GameEntry("SLPM_610.25;1", 0x203F1210, 0x16c00), // Kingdom Hearts I Premium Showcase
             new GameEntry("SLPS_251.05;1", 0x203F2080, 0x16c00), // Kingdom Hearts I (JP)
             new GameEntry("SLUS_203.70;1", 0x203F1C90, 0x16c00), // Kingdom Hearts I (US)
+            new GameEntry("SCUS_203.70;1", 0x203F1C90, 0x16c00), // Kingdom Hearts I (US)
             new GameEntry("SLES_509.67;1", 0x203F22C0, 0x16c00), // Kingdom Hearts I (UK)
             new GameEntry("SLES_509.68;1", 0x203F1AB0, 0x16c00), // Kingdom Hearts I (FR)
             new GameEntry("SLES_509.69;1", 0x203F3CF0, 0x16c00), // Kingdom Hearts I (DE)
             new GameEntry("SLES_509.70;1", 0x203F3900, 0x16c00), // Kingdom Hearts I (IT)
             new GameEntry("SLES_509.71;1", 0x203F4620, 0x16c00), // Kingdom Hearts I (ES)
+            new GameEntry("SCES_509.67;1", 0x203F22C0, 0x16c00), // Kingdom Hearts I (UK)
+            new GameEntry("SCES_509.68;1", 0x203F1AB0, 0x16c00), // Kingdom Hearts I (FR)
+            new GameEntry("SCES_509.69;1", 0x203F3CF0, 0x16c00), // Kingdom Hearts I (DE)
+            new GameEntry("SCES_509.70;1", 0x203F3900, 0x16c00), // Kingdom Hearts I (IT)
+            new GameEntry("SCES_509.71;1", 0x203F4620, 0x16c00), // Kingdom Hearts I (ES)
             new GameEntry("SLPS_251.97;1", 0x203F8380, 0x16c00), // Kingdom Hearts I: Final Mix
             new GameEntry("SLPS_251.98;1", 0x203F8380, 0x16c00), // Kingdom Hearts I: Final Mix
 
             new GameEntry("SLPM_662.33;1", 0x2033DCE0, 0xb830), // Kingdom Hearts II (JP)
             new GameEntry("SLUS_210.05;1", 0x2033E860, 0xb4e0), // Kingdom Hearts II (US)
+            new GameEntry("SCUS_210.05;1", 0x2033E860, 0xb4e0), // Kingdom Hearts II (US)
             new GameEntry("SLES_541.44;1", 0x2033ED60, 0xb4e0), // Kingdom Hearts II (UK)
             new GameEntry("SLES_542.32;1", 0x2033ED60, 0xb4e0), // Kingdom Hearts II (FR)
             new GameEntry("SLES_542.33;1", 0x2033ED60, 0xb4e0), // Kingdom Hearts II (DE)
             new GameEntry("SLES_542.34;1", 0x2033ED60, 0xb4e0), // Kingdom Hearts II (IT)
-            new GameEntry("SLES_542.35;1", 0x2033ED60, 0xb4e0), // Kingdom Hearts II (ES)
+            new GameEntry("SCES_542.35;1", 0x2033ED60, 0xb4e0), // Kingdom Hearts II (ES)
+            new GameEntry("SCES_541.44;1", 0x2033ED60, 0xb4e0), // Kingdom Hearts II (UK)
+            new GameEntry("SCES_542.32;1", 0x2033ED60, 0xb4e0), // Kingdom Hearts II (FR)
+            new GameEntry("SCES_542.33;1", 0x2033ED60, 0xb4e0), // Kingdom Hearts II (DE)
+            new GameEntry("SCES_542.34;1", 0x2033ED60, 0xb4e0), // Kingdom Hearts II (IT)
+            new GameEntry("SCES_542.35;1", 0x2033ED60, 0xb4e0), // Kingdom Hearts II (ES)
             new GameEntry("SLPM_666.75;1", 0x2032BB30, 0x10fc0), // Kingdom Hearts II: Final Mix
         };
 
