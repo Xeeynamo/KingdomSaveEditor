3207131097abb87d2d63ec556a1145e3c84634f2
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-08-25 11:25:09 +0200

    Adds few Persona 5 Royal specific Persona

--- KHSave.LibPersona5/Types/Demon.cs
+++ KHSave.LibPersona5/Types/Demon.cs
@@ -5,10 +5,12 @@ namespace KHSave.LibPersona5.Types
 {
     public class DemonAttribute : InfoAttribute
     {
+        public bool Royal { get; }
         public string Arcana { get; }
 
-        public DemonAttribute(string name = null, string tarot = null) : base(name)
+        public DemonAttribute(string name = null, bool royal = false, string tarot = null) : base(name)
         {
+            Royal =
             Arcana = tarot ?? string.Empty;
         }
 
@@ -17,87 +19,91 @@ namespace KHSave.LibPersona5.Types
     }
     public class MagicianAttribute : DemonAttribute
     {
-        public MagicianAttribute(string name = null) : base(name, "Magician") { }
+        public MagicianAttribute(string name = null, bool royal = false) : base(name, royal, "Magician") { }
     }
     public class JusticeAttribute : DemonAttribute
     {
-        public JusticeAttribute(string name = null) : base(name, "Justice") { }
+        public JusticeAttribute(string name = null, bool royal = false) : base(name, royal, "Justice") { }
     }
     public class DevilAttribute : DemonAttribute
     {
-        public DevilAttribute(string name = null) : base(name, "Devil") { }
+        public DevilAttribute(string name = null, bool royal = false) : base(name, royal, "Devil") { }
     }
     public class StarAttribute : DemonAttribute
     {
-        public StarAttribute(string name = null) : base(name, "Star") { }
+        public StarAttribute(string name = null, bool royal = false) : base(name, royal, "Star") { }
     }
     public class LoversAttribute : DemonAttribute
     {
-        public LoversAttribute(string name = null) : base(name, "Lovers") { }
+        public LoversAttribute(string name = null, bool royal = false) : base(name, royal, "Lovers") { }
     }
     public class ChariotAttribute : DemonAttribute
     {
-        public ChariotAttribute(string name = null) : base(name, "Chariot") { }
+        public ChariotAttribute(string name = null, bool royal = false) : base(name, royal, "Chariot") { }
     }
     public class EmperorAttribute : DemonAttribute
     {
-        public EmperorAttribute(string name = null) : base(name, "Emperor") { }
+        public EmperorAttribute(string name = null, bool royal = false) : base(name, royal, "Emperor") { }
     }
     public class HangedAttribute : DemonAttribute
     {
-        public HangedAttribute(string name = null) : base(name, "Hanged") { }
+        public HangedAttribute(string name = null, bool royal = false) : base(name, royal, "Hanged") { }
     }
     public class FoolAttribute : DemonAttribute
     {
-        public FoolAttribute(string name = null) : base(name, "Fool") { }
+        public FoolAttribute(string name = null, bool royal = false) : base(name, royal, "Fool") { }
     }
     public class TowerAttribute : DemonAttribute
     {
-        public TowerAttribute(string name = null) : base(name, "Tower") { }
+        public TowerAttribute(string name = null, bool royal = false) : base(name, royal, "Tower") { }
     }
     public class HierophantAttribute : DemonAttribute
     {
-        public HierophantAttribute(string name = null) : base(name, "Hierophant") { }
+        public HierophantAttribute(string name = null, bool royal = false) : base(name, royal, "Hierophant") { }
     }
     public class PriestessAttribute : DemonAttribute
     {
-        public PriestessAttribute(string name = null) : base(name, "Priestess") { }
+        public PriestessAttribute(string name = null, bool royal = false) : base(name, royal, "Priestess") { }
     }
     public class StrengthAttribute : DemonAttribute
     {
-        public StrengthAttribute(string name = null) : base(name, "Strength") { }
+        public StrengthAttribute(string name = null, bool royal = false) : base(name, royal, "Strength") { }
     }
     public class EmpressAttribute : DemonAttribute
     {
-        public EmpressAttribute(string name = null) : base(name, "Empress") { }
+        public EmpressAttribute(string name = null, bool royal = false) : base(name, royal, "Empress") { }
     }
     public class SunAttribute : DemonAttribute
     {
-        public SunAttribute(string name = null) : base(name, "Sun") { }
+        public SunAttribute(string name = null, bool royal = false) : base(name, royal, "Sun") { }
     }
     public class MoonAttribute : DemonAttribute
     {
-        public MoonAttribute(string name = null) : base(name, "Moon") { }
+        public MoonAttribute(string name = null, bool royal = false) : base(name, royal, "Moon") { }
     }
     public class JudgementAttribute : DemonAttribute
     {
-        public JudgementAttribute(string name = null) : base(name, "Judgement") { }
+        public JudgementAttribute(string name = null, bool royal = false) : base(name, royal, "Judgement") { }
     }
     public class DeathAttribute : DemonAttribute
     {
-        public DeathAttribute(string name = null) : base(name, "Death") { }
+        public DeathAttribute(string name = null, bool royal = false) : base(name, royal, "Death") { }
     }
     public class TemperAttribute : DemonAttribute
     {
-        public TemperAttribute(string name = null) : base(name, "Temper") { }
+        public TemperAttribute(string name = null, bool royal = false) : base(name, royal, "Temper") { }
     }
     public class HermitAttribute : DemonAttribute
     {
-        public HermitAttribute(string name = null) : base(name, "Hermit") { }
+        public HermitAttribute(string name = null, bool royal = false) : base(name, royal, "Hermit") { }
     }
     public class FortuneAttribute : DemonAttribute
     {
-        public FortuneAttribute(string name = null) : base(name, "Fortune") { }
+        public FortuneAttribute(string name = null, bool royal = false) : base(name, royal, "Fortune") { }
+    }
+    public class WorldAttribute : DemonAttribute
+    {
+        public WorldAttribute(string name = null, bool royal = false) : base(name, royal, "World") { }
     }
 
     public enum Demon
@@ -272,7 +278,7 @@ namespace KHSave.LibPersona5.Types
         [Empress] Hariti,
         [Sun] Yurlungur,
         [Hanged] Hecatoncheires,
-        [Unused] Demon170,
+        [Fool("Satanel", true)] Demon170,
         [Unused] Demon171,
         [Unused] Demon172,
         [Unused] Demon173,
@@ -411,5 +417,7 @@ namespace KHSave.LibPersona5.Types
         [Unused] Demon306,
         [Unused] Demon307,
         [Devil] Pazuzu,
+        [World("Izanagi-no-Okami", true)] IzanagiNoOkami = 360,
+        [Fool("Orpheus F Picaro", true)] OrpheusFPicaro = 371,
     }
 }
