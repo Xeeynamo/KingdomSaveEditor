55c216b8e00d7a2518a2e3131c64d457e8121947
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-08-27 18:58:53 +0200

    Reworked pack script

--- pack.bat
+++ pack.bat
@@ -1,2 +1,3 @@
+cd D:\Repository\KH3SaveEditor\KHSave.SaveEditor\bin\Release\net472\win7-x86\
 rm KingdomSaveEditor.exe
-"%ProgramFiles%\WinRAR\WinRAR.exe" a -afrar -cfg- -ed -k -m5 -tl -s -zpack-settings.txt -iicon"KHSave.SaveEditor\Icon1.ico" "-sfx%ProgramFiles%\WinRAR\Default64.sfx" -ep "KingdomSaveEditor.exe" "D:\Repository\KH3SaveEditor\KHSave.SaveEditor\bin\Release\net472\win7-x86\*.exe" "D:\Repository\KH3SaveEditor\KHSave.SaveEditor\bin\Release\net472\win7-x86\*.dll" "D:\Repository\KH3SaveEditor\KHSave.SaveEditor\bin\Release\net472\win7-x86\KHSave.SaveEditor.exe.config"
+"%ProgramFiles%\WinRAR\WinRAR.exe" a -afrar -cfg- -ed -k -m5 -tl -s -z..\..\..\..\..\pack-settings.txt -iicon"D:\Repository\KH3SaveEditor\KHSave.SaveEditor\Icon1.ico" "-sfx%ProgramFiles%\WinRAR\Default64.sfx" "KingdomSaveEditor.exe" "*.exe" "*.dll" "*.exe.config" ".\Resources"
