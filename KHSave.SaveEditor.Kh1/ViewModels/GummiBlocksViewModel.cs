41b559b66f52d35df9a7ad379083227cd83034ba
Christophe Huybrechts <huybchris@hotmail.com>
2021-01-02 22:27:45 +0100

    Add more GummiShip UI

---
+++ KHSave.SaveEditor.Kh1/ViewModels/GummiBlocksViewModel.cs
@@ -0,0 +1,24 @@
+﻿using KHSave.Lib1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xe.Tools.Wpf.Models;
+
+namespace KHSave.SaveEditor.Kh1.ViewModels
+{
+    public class GummiBlocksViewModel : GenericListModel<GummiBlockViewModel>
+    {
+
+        //private readonly GummiBlock[] gummiblocks;
+
+        public GummiBlocksViewModel(GummiBlock[] gummiBlocks) :
+            base(gummiBlocks.Select(x => new GummiBlockViewModel(x)))
+        {
+            //this.gummiblocks = gummiBlocks;
+        }
+
+       
+    }
+}
